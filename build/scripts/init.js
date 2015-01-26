b2332f56d54483ceca6bcb374be17af59ca028e1
mare hunt <mare.hunt@netgroup.ee>
2015-01-26 17:56:28 +0200

    minor changes in new case

--- build/scripts/init.js
+++ build/scripts/init.js
@@ -4,7 +4,22 @@
 
     $('.collapsible').collapsible();
 
+    $('#caseForm .collapse').collapse('show');
+    $('#caseCard').collapse('hide');
+    $('#toggleRepresentative').collapse('hide');
+
     //$(".button-collapse").sideNav();
+
+  // $("#chooseID").joyride({
+  //       autoStart : true,
+  //         postStepCallback : function (index, tip) {
+  //         if (index == 2) {
+  //           $(this).joyride('set_li', false, 1);
+  //         }
+  //       },
+  //       modal:true,
+  //       expose: true
+  // });
 
     // wow.js init
     new WOW().init();
